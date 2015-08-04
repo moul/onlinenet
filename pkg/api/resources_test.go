88b1c5ca0628aebe280d21866147dd5e469f2fcb
Manfred Touron <m@42.am>
2015-08-04 19:56:04 +0200

    Added test

---
+++ pkg/api/resources_test.go
@@ -0,0 +1,24 @@
+package api
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestUnmarshallServer(t *testing.T) {
+	buff := []byte(`{"id":424242,"offer":"Dedibox XXL","hostname":"dedibox-ftw","os":{"name":"ubuntu","version":"14.04_LTS-server"},"power":"ON","boot_mode":"normal","last_reboot":"2014-09-15T11:04:49.000Z","anti_ddos":true,"hardware_watch":true,"proactive_monitoring":true,"support":"Basic service level","abuse":"mail@example.com","location":{"datacenter":"DC3","room":"4","zone":"4-6","line":"C","rack":12,"block":"K","position":4},"network":{"ip":["1.2.3.4"],"private":[],"ipfo":["5.6.7.8"]},"ip":[{"address":"1.2.3.4","type":"public","reverse":"dedibox-ftw.dedibox-fan.fr.","mac":"12:34:56:78:9a:bc","switch_port_state":"up"},{"address":"5.6.7.8","type":"failover","reverse":null,"mac":null,"destination":"1.2.3.4","server":{"$ref":"\/api\/v1\/server\/424242"},"status":"active"}],"contacts":{"owner":"dedibox-fan","tech":"dedibox-fan"},"disks":[{"$ref":"\/api\/v1\/server\/hardware\/disk\/242424"}],"drive_arrays":[{"disks":[{"$ref":"\/api\/v1\/server\/hardware\/disk\/242424"}]}],"bmc":{"session_key":null}}`)
+
+	var server Server
+	err := json.Unmarshal(buff, &server)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if server.Hostname != "dedibox-ftw" {
+		t.Fatalf("Expected server.Hostname=dedibox-ftw got %s", server.Hostname)
+	}
+
+	if server.Os.Name != "ubuntu" {
+		t.Fatalf("Expected server.Os.Name=ubuntu got %s", server.Os.Name)
+	}
+}
